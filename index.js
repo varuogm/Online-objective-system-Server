2b735cdf17c1e4221d232c9df2d5e3a1ff0ca969
gourav majee <44859494+varuogm@users.noreply.github.com>
2021-07-17 15:45:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT =5000;
+const PORT = process.env.PORT ||    5000;
 
 app.use(cors());
 app.use(bodyParser.json());
