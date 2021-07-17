07baaa5741e1f52881f593f6e4dbf82f0a64df9d
gourav majee <44859494+varuogm@users.noreply.github.com>
2021-07-17 16:11:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = process.env.PORT ||    5000;
+const PORT = process.env.PORT ||  5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 
 
-app.listen( 5000, function() {
+app.listen( PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
 
